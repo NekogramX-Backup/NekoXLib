b1f081ef9a024c628852184876784b0b45990e31
世界 <i@nekox.me>
2020-03-06 20:41:41 +0800

    喵

--- src/main/java/nekox/core/client/TdCliClient.kt
+++ src/main/java/nekox/core/client/TdCliClient.kt
@@ -1,5 +1,10 @@
 package nekox.core.client
 
+import cn.hutool.core.lang.Console
+import nekox.core.defaultLog
+import nekox.core.raw.checkAuthenticationCode
+import nekox.core.raw.checkAuthenticationPassword
+import nekox.core.raw.setAuthenticationPhoneNumber
 import td.TdApi
 
 open class TdCliClient(options: TdOptions) : TdClient(options) {
@@ -10,6 +15,74 @@ open class TdCliClient(options: TdOptions) : TdClient(options) {
     override suspend fun onAuthorizationState(authorizationState: TdApi.AuthorizationState) {
 
         super.onAuthorizationState(authorizationState)
+
+        if (authorizationState is TdApi.AuthorizationStateWaitPhoneNumber) {
+
+            while (true) {
+
+                Console.print("输入手机号: ")
+
+                val phone = Console.input()
+
+                try {
+
+                    setAuthenticationPhoneNumber(phone)
+
+                    break
+
+                } catch (e: TdException) {
+
+                    defaultLog.error("手机号无效: ${e.message}")
+
+                }
+
+            }
+
+        } else if (authorizationState is TdApi.AuthorizationStateWaitCode) {
+
+            while (true) {
+
+                Console.print("输入验证码: ")
+
+                val code = Console.input()
+
+                try {
+
+                    checkAuthenticationCode(code)
+
+                    break
+
+                } catch (e: TdException) {
+
+                    defaultLog.error("验证码错误: ${e.message}")
+
+                }
+
+            }
+
+        } else if (authorizationState is TdApi.AuthorizationStateWaitPassword) {
+
+            while (true) {
+
+                Console.print("输入密码: ")
+
+                val pswd = Console.input()
+
+                try {
+
+                    checkAuthenticationPassword(pswd)
+
+                    break
+
+                } catch (e: TdException) {
+
+                    defaultLog.error("密码错误: ${e.message}")
+
+                }
+
+            }
+
+        }
 
     }
 
